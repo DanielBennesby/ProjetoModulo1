b5623caf55f80e308051919ef23420c3387076c4
Daniel Bennesby <danielbennesby@gmail.com>
2022-01-18 15:03:10 -0300

    alteração de JS

--- Ciclo.js
+++ Ciclo.js
@@ -19,3 +19,131 @@ function heroi(){
       } 
   }
 }
+
+function vampiro3(){
+  while(x != 1 || x!= 2){
+   var x = Number(prompt("Escolha"))
+    if( x === 1){
+        window.location.href ="./3ª morte do vampiro.html";
+      break
+    }
+    else if(x === 2){
+        window.location.href = "./4º slide do vampiro.html"
+      break
+    }
+    else{
+      alert("Digite 1 ou 2, como foi pedido");
+      
+    } 
+  }
+}
+
+function vampiro2(){
+  while(x != 1 || x!= 2){
+   var x = Number(prompt("Escolha"))
+    if( x === 1){
+        window.location.href ="./2ª morte do vampiro.html";
+      break
+    }
+    else if(x === 2){
+        window.location.href = "./3ª pagina do Vampiro.html"
+      break
+    }
+    else{
+      alert("Digite 1 ou 2, como foi pedido");
+      
+    } 
+  }
+}
+
+function vampiro1() {
+  while (x != 1 || x != 2) {
+      var x = Number(prompt("Escolha"))
+      if (x === 1) {
+          window.location.href = "./1ª morte do vampiro.html";
+          break
+      }
+      else if (x === 2) {
+          window.location.href = "./2ª pagina do Vampiro.html"
+          break
+      }
+      else {
+          alert("Digite 1 ou 2, como foi pedido");
+          
+      }
+  }
+}
+
+function paladino2(){
+  while(x != 1 || x!= 2){
+   var x = Number(prompt("Escolha"))
+    if( x === 1){
+        window.location.href ="./2ª Morte Paladino.html";
+      break
+    }
+    else if(x === 2){
+        window.location.href = "./3ª pagina Paladino.html"
+      break
+    }
+    else{
+      alert("Digite 1 ou 2, como foi pedido");
+      
+    } 
+}
+}
+
+function paladino1(){
+  while(x != 1 || x!= 2){
+   var x = Number(prompt("Escolha"))
+    if( x === 1){
+        window.location.href ="./2ª pagina do Paladino.html";
+      break
+    }
+    else if(x === 2){
+        window.location.href = "./1ª Morte Paladino.html"
+      break
+    }
+    else{
+      alert("Digite 1 ou 2, como foi pedido");
+      
+    } 
+}
+}
+
+function aprendiz2() {
+  while (x != 1 || x != 2) {
+      var x = Number(prompt("Escolha"))
+      if (x === 1) {
+          window.location.href = "./2ª morte do Aprendiz.html";
+          break
+      }
+      else if (x === 2) {
+          window.location.href = "./3ª pagina Aprendiz.html"
+          break
+      }
+      else {
+          alert("Digite 1 ou 2, como foi pedido");
+          
+      }
+  }
+}
+
+function aprendiz1() {
+  while (x != 1 || x != 2) {
+      var x = Number(prompt("Escolha"))
+      if (x === 1) {
+          window.location.href = "./2ª pagina do aprendiz.html";
+          break
+      }
+      else if (x === 2) {
+          window.location.href = "./1ª morte do Aprendiz.html"
+          break
+      }
+      else {
+          alert("Digite 1 ou 2, como foi pedido");
+          
+      }
+  }
+}
+
+
